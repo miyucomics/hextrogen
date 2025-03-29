203f0cb8d224d981fea19f7efd8abdc38a125024
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-03-28 21:44:01 -0400

    First Hextrogen update!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
 # Changelog
 
+## 1.1.0
+- changed page to use the glorious strawberry
+- changed Overdrive spell to take in a player
+  - this means you can make proper Celeste orbs that reset your dash with wisps!
+- fixed small grammatical issue in docs
+
 ## 1.0.0
 - initial release!
--- src/main/java/miyucomics/hextrogen/OpRefreshDashes.kt
+++ src/main/java/miyucomics/hextrogen/OpRefreshDashes.kt
@@ -3,23 +3,21 @@ package miyucomics.hextrogen
 import at.petrak.hexcasting.api.casting.RenderedSpell
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.getPlayer
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.mishaps.MishapBadCaster
 import at.petrak.hexcasting.api.misc.MediaConstants
 import dev.mayaqq.estrogen.registry.EstrogenEffects
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.server.level.ServerPlayer
-import net.minecraft.world.entity.player.Player
 
 class OpRefreshDashes : SpellAction {
-	override val argc = 0
+	override val argc = 1
 	override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
-		if (env.castingEntity !is Player)
-			throw MishapBadCaster()
-		if (!env.castingEntity!!.hasEffect(EstrogenEffects.ESTROGEN_EFFECT.get()))
+		val player = args.getPlayer(0, argc)
+		if (!player.hasEffect(EstrogenEffects.ESTROGEN_EFFECT.get()))
 			throw NoEstrogenMishap()
-		return SpellAction.Result(Spell(env.castingEntity as ServerPlayer), MediaConstants.CRYSTAL_UNIT, listOf())
+		return SpellAction.Result(Spell(player), MediaConstants.CRYSTAL_UNIT, listOf())
 	}
 
 	private data class Spell(val player: ServerPlayer) : RenderedSpell {
